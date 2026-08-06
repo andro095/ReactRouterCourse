5ef4cf653cc09ab2c7c5ea110c33a4cd18aa30d6
andro095 <andre.rodriguez.gt@hotmail.com>
2026-08-05 22:50:15 -0400

    feat: implement form submission with clientAction and serverAction integration on testing page

--- support-chat-framework/app/routes/auth/testing-page.tsx
+++ support-chat-framework/app/routes/auth/testing-page.tsx
@@ -1,6 +1,37 @@
 import { useEffect, useState } from "react"
-import { Link, NavLink } from "react-router"
+import { Form, Link, NavLink, useNavigation } from "react-router"
 import type { Route } from "./+types/testing-page"
+import { sleep } from "~/lib/sleep";
+
+export async function action({ request }: Route.ActionArgs) {
+
+    await sleep(2000);
+
+    const data = await request.formData();
+    console.log("Server side action");
+    console.log({ data });
+    return {
+        ok: true,
+        data,
+        message: "Data was submitted successfully",
+    };
+}
+
+export async function clientAction({ serverAction, request }: Route.ClientActionArgs) {
+    await sleep(1500);
+    // can still call the server action if needed
+    const formData = await request.clone().formData();
+    const allData = Object.fromEntries(formData);
+    console.log(allData, "from client action form data")
+
+    const data = await serverAction();
+
+    return {
+        ok: true,
+        data: allData,
+        message: "Data was submitted successfully from client action",
+    };
+}
 
 export const loader = async () => {
     console.log("Hello, world from the server loader!")
@@ -51,6 +82,11 @@ export default function TestingPage({
 }: Route.ComponentProps) {
     const [isHydrated, setIsHydrated] = useState(false)
 
+    const navigation = useNavigation();
+    const isPosting = navigation.state === "submitting";
+
+    console.log({ navigation, isPosting })
+
     useEffect(() => {
         setIsHydrated(true)
     }, [])
@@ -80,6 +116,31 @@ export default function TestingPage({
             >
                 Testing Args
             </NavLink>
+
+            <Form
+                className="mt-2 flex gap-2"
+                method="post"
+            >
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    className="border border-slate-300 rounded px-2 py-1"
+                />
+                <input
+                    type="text"
+                    name="age"
+                    placeholder="Age"
+                    className="border border-slate-300 rounded px-2 py-1"
+                />
+                <button
+                    disabled={isPosting}
+                    type="submit"
+                    className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                    {isPosting ? "Posting..." : "Submit"}
+                </button>
+            </Form>
         </div>
     )
 }
